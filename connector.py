013702f87d34eceea22ebff616d205ef955d13fc
rein <64058049+rlategan@users.noreply.github.com>
2023-09-08 18:18:28 +1000

    Add worded difficulty levels, clean up code

--- connector.py
+++ connector.py
@@ -4,19 +4,19 @@ from urllib.request import urlopen
 
 start_date = "2023-06-12"
 todays_date = str(date.today())
+delta = datetime.strptime(todays_date, "%Y-%m-%d") - datetime.strptime(start_date, "%Y-%m-%d")
+id = data[delta.days]['id']
 
 url = "https://www.nytimes.com/games-assets/connections/game-data-by-day.json"
 response = urlopen(url)
 data = json.loads(response.read())
 
-delta = datetime.strptime(todays_date, "%Y-%m-%d") - datetime.strptime(start_date, "%Y-%m-%d")
-id = data[delta.days]['id']
-#print(delta.days)
-
 groups_list = list(data[delta.days]['groups'])
 items_list = list(data[delta.days]['groups'].values())
 
+difficulties = ['straightforward','intermediate','hard', 'tricky']
+
 for i in range(0,4):
-    print('level: ' + str(items_list[i]['level']))
+    print('difficulty: ' + str(difficulties[i]))
     print(groups_list[i] + ":\n" + ', '.join(items_list[i]['members']) + "\n")
 
